14ba5fd3fe46dcc1b8975886cf3b179b8c8fd222
SongJunSub <s911130@naver.com>
2024-03-27 20:07:27 +0900

    Front-End 구현 1. npm 설치  - React JS  - axios 2. 화면 구현 3. CSS 작업

---
+++ frontend/src/App.js
@@ -0,0 +1,62 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from "axios";
+
+function App() {
+
+  const [lists, setLists] = useState([])
+  const [value, setValue] = useState("")
+
+  useEffect(() => {
+    axios.get('/api/selectValues')
+        .then(response => {
+          console.log('response', response)
+          setLists(response.data)
+        })
+  }, [])
+
+  const changeHandler = (event) => {
+    setValue(event.currentTarget.value)
+  }
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    axios.post('/api/insertValue', {value: value})
+        .then(response => {
+          if(response.data.success){
+            console.log('response', response)
+            setLists([...lists, response.data])
+            setValue("");
+          }
+          else{
+            alert('Inserting Failed')
+          }
+        })
+  }
+
+  return (
+      <div className="App">
+        <header className="App-header">
+            <h3>Multi Container Application</h3>
+            <img src={logo} className="App-logo" alt="logo"/>
+            <div className="container">
+                {lists && lists.map((list, index) => (
+                    <li key={index}>{list.value}</li>
+                ))}
+
+                <br/>
+
+                <form className="example" onSubmit={submitHandler}>
+                    <input type="text" placeholder="입력해주세요..." value={value} onChange={changeHandler}/>
+                    <button type="submit">등록</button>
+                </form>
+            </div>
+        </header>
+      </div>
+  );
+
+}
+
+export default App;
