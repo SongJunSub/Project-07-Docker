b168a9d11748e4e67e0607dfa4e99344d66dcbac
SongJunSub <s911130@naver.com>
2024-03-27 21:36:52 +0900

    Fix bug

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,57 +5,57 @@ import axios from 'axios';
 
 function App() {
 
-  const [lists, setLists] = useState([])
-  const [value, setValue] = useState("")
+    useEffect(() => {
+        axios.get('/api/selectValues')
+            .then(response => {
+                console.log('response', response)
+                setLists(response.data)
+            })
+    }, [])
 
-  useEffect(() => {
-    axios.get('/api/selectValues')
-        .then(response => {
-          console.log('response', response)
-          setLists(response.data)
-        })
-  }, [])
+    const [lists, setLists] = useState([])
+    const [value, setValue] = useState("")
 
-  const changeHandler = (event) => {
-    setValue(event.currentTarget.value)
-  }
+    const changeHandler = (event) => {
+        setValue(event.currentTarget.value)
+    }
 
-  const submitHandler = (event) => {
-    event.preventDefault();
+    const submitHandler = (event) => {
+        event.preventDefault();
 
-    axios.post('/api/insertValue', {value: value})
-        .then(response => {
-          if(response.data.success){
-            console.log('response', response)
-            setLists([...lists, response.data])
-            setValue("");
-          }
-          else{
-            alert('Inserting Failed')
-          }
-        })
-  }
+        axios.post('/api/insertValue', {value: value})
+            .then(response => {
+              if(response.data.success){
+                  console.log('response', response)
+                  setLists([...lists, response.data])
+                  setValue("");
+              }
+              else{
+                  alert('Inserting Failed')
+              }
+            })
+    }
 
-  return (
-      <div className="App">
-        <header className="App-header">
-            <h3>Multi Container Application</h3>
-            <img src={logo} className="App-logo" alt="logo"/>
-            <div className="container">
-                {lists && lists.map((list, index) => (
-                    <li key={index}>{list.value}</li>
-                ))}
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h3>Multi Container Application</h3>
+                <img src={logo} className="App-logo" alt="logo"/>
+                <div className="container">
+                    {lists && lists.map((list, index) => (
+                        <li key={index}>{list.value}</li>
+                    ))}
 
-                <br/>
+                    <br/>
 
-                <form className="example" onSubmit={submitHandler}>
-                    <input type="text" placeholder="입력해주세요..." value={value} onChange={changeHandler}/>
-                    <button type="submit">등록</button>
-                </form>
-            </div>
-        </header>
-      </div>
-  );
+                    <form className="example" onSubmit={submitHandler}>
+                        <input type="text" placeholder="입력 시 데이터 저장 후 상단에 리스트로 표기됩니다." value={value} onChange={changeHandler}/>
+                        <button type="submit">등록</button>
+                    </form>
+                </div>
+            </header>
+        </div>
+    );
 
 }
 
